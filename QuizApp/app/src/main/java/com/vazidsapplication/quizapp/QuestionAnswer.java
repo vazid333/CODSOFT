ac5719b267fee76baae80447be5d6f64fe4e62ed
vazid333 <143502039+vazid333@users.noreply.github.com>
2023-09-30 01:37:09 +0530

    Add files via upload

---
+++ QuizApp/app/src/main/java/com/vazidsapplication/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.vazidsapplication.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "Which programming language is used in Android Studio?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Python","Go","Kotlin","HTML"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "Kotlin"
+    };
+
+}
